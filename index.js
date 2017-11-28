41883c3c85c0a8a3a5c4243dafe93ebe6006c337
Daniel Meilleur <daniel.meilleur@gmail.com>
2017-11-28 15:31:11 -0800

    finalisation output format, procfile

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ const express = require('express')
 
 var app = express()
 const useragent = require('express-useragent')
-// const util = require('util')
 
-var port = 3000
+const PORT = process.env.PORT || 5000
 
 app.use(useragent.express());
 app.get('/', (req, res) => {
     var lang = req.
-    res.send('ipaddress: ' + req.ip + ',langage: ' + 
-            req.headers["accept-language"].split(',')[0] + 
-            ',software:' + req.useragent.os)
-})
+    res.send({
+        'ipaddress': req.ip, 
+        'langage': req.headers["accept-language"].split(',')[0],
+        'software': req.useragent.os
+        })
+    })
 
-app.listen(port, () => {
-    console.log('server listening on port ' + port)
-})
+app.listen(PORT, () => console.log(`server listening on port ${PORT}`) )
