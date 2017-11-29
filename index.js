c8db147b24e26d5eeadd3d59dc6d60fa364f7269
Daniel Meilleur <daniel.meilleur@gmail.com>
2017-11-28 19:39:28 -0800

    ip avec choix de 4 stratégies selon site de Keyang Xiang

--- index.js
+++ index.js
@@ -6,9 +6,14 @@ const useragent = require('express-useragent')
 const PORT = process.env.PORT || 5000
 
 app.use(useragent.express());
+
 app.get('/', (req, res) => {
+    var ip = (req.headers['x-forwarded-for'] ||
+            req.connection.remoteAddress ||
+            req.socket.remoteAddress ||
+            req.connection.socket.remoteAddress).split(",")[0];
     res.send({
-        'ipaddress': req.connection.remoteAddress, 
+        'ipaddress': ip,
         'langage': req.headers["accept-language"].split(',')[0],
         'software': req.useragent.os
         })
